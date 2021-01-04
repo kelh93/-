0b9c0d92b62e74cc11c451a06b14b24fb92a7113
kelh <kelh93@163.com>
2021-01-05 07:29:04 +0800

    手写promise

---
+++ myPromise.js
@@ -0,0 +1,56 @@
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
+
+class MyPromise {
+  // executor： promise接受的回调函数，包含resolve，reject 2个参数，并且这2个参数都是函数
+  constructor(executor) {
+    this.status = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+
+    this.onResolvedCallbacks = [];
+    this.onRejectedCallbacks = [];
+
+    let resolve = (value) => {
+      if (this.status === PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+        this.onResolvedCallbacks.push((fn) => fn());
+      }
+    };
+
+    let reject = (reason) => {
+      if (this.status === PENDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        this.onRejectedCallbacks.push((fn) => fn());
+      }
+    };
+
+    // promise内部使用try，catch捕获异常。catch使用reject返回。
+    try {
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e);
+    }
+  }
+
+  //promise链式调用的then，then有2个参数，分别是执行成功和执行失败的回调
+  then(onFulfilled, onRejected) {
+    if (this.status === FULFILLED) {
+      onFulfilled(this.value);
+    }
+    if (this.status === REJECTED) {
+      onRejected(this.reason);
+    }
+    if (this.status === PENDING) {
+      this.onResolvedCallbacks.push(() => {
+        onFulfilled(this.value);
+      });
+      this.onRejectedCallbacks.push(() => {
+        onrejectionhandled(this.reason);
+      });
+    }
+  }
+}
